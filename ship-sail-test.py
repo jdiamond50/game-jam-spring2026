34649a83d2b6f19e97020347ed7f28daf429aa2a
Julien Diamond <juliendiamond@dhcp-10-5-128-158.wireless.rochester.edu>
2026-04-11 20:23:54 -0400

    deleted offscreen ships and added some comments because i'm sane

--- ship-sail-test.py
+++ ship-sail-test.py
@@ -1,55 +1,56 @@
 import pygame
 import random
 
-class Ship(pygame.sprite.Sprite):
+class Ship(pygame.sprite.Sprite): 
     def __init__(self, y_val):
-        super().__init__()
-        image = pygame.image.load('ship.png')
-        self.image = pygame.transform.scale(image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (0,y_val)
+        super().__init__() 
+        image = pygame.image.load('ship.png') 
+        self.image = pygame.transform.scale(image, (50, 50)) 
+        self.rect = self.image.get_rect() # gets rect = (x,y,width,height)
+        self.rect.topright = (0,y_val)
     
     def update(self):
         self.rect.x += 1
+        if self.rect.left > WIDTH: # remove offscreen ships
+            self.kill()
 
-# time stuff
+ships = pygame.sprite.Group()
 
 pygame.init()
-clock = pygame.time.Clock()
-framerate = 60
-next_ship_time = random.randint(60, 120)
+
+# create screen
 
 WIDTH = 400
 HEIGHT = 300
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-ships = pygame.sprite.Group()
+
+# time stuff
+clock = pygame.time.Clock()
+framerate = 60
+next_ship_time = random.randint(framerate, 2*framerate) # [1 second, 2 seconds]
 
 run = True
 while run:
     clock.tick(framerate)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: 
             run = False
-
-    # update stuff (mathematically)
     
-    if (next_ship_time == 0):
+    if (next_ship_time == 0): # time to create another ship
         new_ship = Ship(random.randint(0, HEIGHT-50))
         ships.add(new_ship)
-        next_ship_time = random.randint(60,120)
+        next_ship_time = random.randint(60,120) # set countdown for next ship
+
     next_ship_time -= 1
 
-    prev_time = pygame.time.get_ticks()
-
-    ships.update()
+    ships.update() # move ships to the right
 
     # draw stuff
 
-    screen.fill((0, 157, 255))
+    screen.fill((0, 157, 255)) # light blue
     ships.draw(screen)
 
-    pygame.display.flip()
+    pygame.display.flip() # update screen
 
 pygame.quit()
